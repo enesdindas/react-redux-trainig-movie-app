2836508393c6da38463016830896f463cefd31af
Enes <enesdindas26@gmail.com>
2019-02-26 21:31:08 +0300

    Creating Store and Root Reducer; installing and set Thunk Middleware, Redux, React Router; creating and set connections of MoviesPage component and MoviesList component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+
+import MoviesPage from './components/pages/MoviesPage';
+import { Link,Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Link to="movies">Movies</Link>
+        <Route path='/movies' component={MoviesPage}></Route>
       </div>
     );
-  }
-}
+  };
+};
 
 export default App;
