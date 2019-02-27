2613b9f868d7d4e50486f4650c4708796b759336
Enes <enesdindas26@gmail.com>
2019-02-27 16:57:07 +0300

    add and set semantic theme

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import React, { Component } from 'react';
-//import logo from './logo.svg';
 import './App.css';
-
+import 'semantic-ui-css/semantic.min.css';
+import { Container } from 'semantic-ui-react'
 import MoviesPage from './components/pages/MoviesPage';
-import { Link,Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 class App extends Component {
+  state = {
+  };
+
+  stickOverlay = () => this.setState({ overlayFixed: true })
+  unStickOverlay = () => this.setState({ overlayFixed: false })
+
   render() {
     return (
       <div className="App">
-        <Link to="movies">Movies</Link>
-        <Route path='/movies' component={MoviesPage}></Route>
+        
+        <Header/>
+
+        <Container text>
+
+          <Route path='/movies' component={MoviesPage}></Route>
+          <hr/>
+        </Container>
+
+        <Footer/>
+
+       
       </div>
     );
   };
